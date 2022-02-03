44fb724c2404d9c2e28d7fc0b81d8da01abb0194
Elvira Cañas <elvfira@gmail.com>
2022-02-03 13:29:09 +0100

    update body params static server

--- src/index.js
+++ src/index.js
@@ -9,7 +9,11 @@ const server = express();
 
 // Configuramos el servidor
 server.use(cors());
-server.use(express.json());
+server.use(
+  express.json({
+    limit: "10mb",
+  })
+);
 
 // Arrancamos el servidor en el puerto 3000
 const serverPort = 4000;
@@ -17,21 +21,32 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
+const saveCards = [];
+
 // Escribimos los endpoints que queramos
 server.post("/card", (req, res) => {
-  console.log(req.body)
+  console.log(req.body);
   const responseSuccess = {
-    "success": true,
-    "cardURL": "https://awesome-profile-cards.herokuapp.com/${req.body.uuid}"
+    success: true,
+    cardURL: `https://awesome-profile-cards.herokuapp.com/${req.body.uuid}`,
   };
 
   const responseError = {
-    "success": false,
-    "error": "Error description"
+    success: false,
+    error: "Error description",
+  };
+
+  if (req.body.name !== "" && req.body.job !== "" &&  req.body.linkedin !== "" && req.body.email !== "" && req.body.photo !== "" && req.body.github !== "") {
+    saveCards.push(req.body);
+    res.json(responseSuccess);
+  } else {
+    res.json(responseError);
   }
-  res.json(responseSuccess);
 });
 
 server.get("/card", (req, res) => {
-  res.json("holi")
-})
+  //res.json("holi")
+});
+
+const staticServerPathWeb = "./src/public-react";
+server.use(express.static(staticServerPathWeb));
